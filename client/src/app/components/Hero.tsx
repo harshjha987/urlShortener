42ca5bf61d335ffb29927fa3e2f31e004ce5f2bb
hrjha97 <jha.harsh837@gmail.com>
2025-02-11 02:48:05 +0530

    added ui

---
+++ client/src/app/components/Hero.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { Highlight,HeroHighlight } from "./ui/hero-highlight";
+
+import { Button } from "./ui/moving-border";
+function Hero() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col 
+    items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <HeroHighlight>
+     
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="text-2xl mt-10  px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+      >
+         Shortrix, The professional URL shortening service.{" "}
+         <br />
+        <Highlight className="text-black dark:text-white">
+        Shorten Your Links, Simplify Your Sharing!
+        </Highlight>
+      </motion.h1>
+      <div className="z-10 text-center mt-6">
+      <Button
+    borderRadius="1.75rem"
+    className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200
+     mx-auto dark:border-slate-800 z-10 "
+  >
+    Explore now.
+  </Button>
+  </div>
+    </HeroHighlight>
+    
+   
+  </div>
+  );
+}
+export default Hero
---
+++ client/src/app/components/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import logo from "../images/logo-url.png"
+import Image from "next/image";
+import { AlignJustify,X } from 'lucide-react';
+
+function Navbar({ className }: { className?: string }){
+    const [active, setActive] = useState<string | null>(null);
+    const [open,setOpen] = useState(false);
+    return (
+        <div
+          className={cn("fixed top-6 inset-x-0 max-w-full mx-auto z-50   justify-between ", className)}
+        >
+            
+            <Menu setActive={setActive}>
+                <div className="flex font-bold ml-5 items-center">
+                    <Image src = {logo} alt="/logo" className="h-6 w-6 mr-2"/>
+                   <p> Shortrix</p> 
+                </div>
+            
+            <div className="hidden md:flex items-center space-x-6">
+                <Link href= "/" >
+        <MenuItem setActive={setActive} active={active} item="Home">
+          
+        </MenuItem>
+        </Link>
+        
+        <MenuItem setActive={setActive} active={active} item="Service">
+         
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Contact us">
+          
+        </MenuItem>
+        </div>
+        <div className=" hidden md:flex items-center">
+        <Link href={'/'} className="rounded-lg  px-4  bg-blue-600">
+        <MenuItem setActive={setActive} active={active} item="Sign Up">
+          
+        </MenuItem>
+        </Link>
+        </div>
+        <button onClick={() => setOpen(!open)} className="md:hidden">
+          <AlignJustify  />
+        </button>
+      
+        {open && (
+        <div className="md:hidden flex flex-col space-y-4 mt-4">
+          <Link href="/">
+            <MenuItem setActive={setActive} active={active} item="Home" />
+          </Link>
+          <MenuItem setActive={setActive} active={active} item="Service" />
+          <MenuItem setActive={setActive} active={active} item="Contact us" />
+          <Link href="/" className="rounded-lg px-4 py-2 bg-blue-600 text-white">
+            <MenuItem setActive={setActive} active={active} item="Sign Up" />
+          </Link>
+        </div>
+      )}
+
+        </Menu>
+        </div>
+    )
+
+}
+
+export default Navbar;
